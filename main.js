f0805b4899b95f3dc136effc0ca6e649213f4b0c
Griffin0-0 <griffinn971@stemia.ca>
2022-12-28 17:33:40 -0700

    test font size

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ var mobileMenu = document.getElementById("mobile-menu-container")
 function toggleMobileMenu(){
     mobileMenu.classList.toggle("open")
 }
+
+function toggleElement(el) {
+    el.classList.toggle("open")
+}
