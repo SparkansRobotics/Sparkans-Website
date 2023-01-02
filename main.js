d19025d3087d0721b526aae40f4582d840538999
Griffin0-0 <114698399+Griffin0-0@users.noreply.github.com>
2023-01-02 00:56:28 -0700

    Large - Home page change

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+// Typing effect "Next Generation"
+
+var char = 0;
+var text = 'Next Generation';
+var speed = 70;
+var scrolled = false;
+
+function typeEffect() {
+        if (char < text.length) {
+            document.getElementById("type-effect").innerHTML += text.charAt(char);
+            char++;
+            setTimeout(typeEffect, speed);
+        }
+}
+
+var myElement = document.getElementById('type-effect');
+
+document.addEventListener("scroll", () =>  {
+    var bounding = myElement.getBoundingClientRect();
+    
+    if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= window.innerWidth && bounding.bottom <= window.innerHeight) {
+        if (scrolled != true) {
+            typeEffect();
+            scrolled = true;
+        }
+    }
+})
+
 var mobileMenu = document.getElementById("mobile-menu-container")
 
 function toggleMobileMenu(){
