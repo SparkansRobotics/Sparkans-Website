dd761a667ff04e935a1ed73935c716cc388a563d
Griffin0-0 <114698399+Griffin0-0@users.noreply.github.com>
2023-01-05 01:33:04 -0700

    Parallax image effect - sponsor page (unfinished)

---
+++ home.js
@@ -0,0 +1,65 @@
+// Javascript
+
+// Typing effect "Next Generation"
+
+var char = 0;
+var text = 'Innovators';
+var speed = 70;
+
+function typeEffect() {
+    if (char < text.length) {
+        document.getElementById("type-effect").innerHTML += text.charAt(char);
+        char++;
+        setTimeout(typeEffect, speed);
+    }
+}
+
+
+
+// Parallax
+
+function parallax() {
+	var pLowUp = document.getElementById("parallax-lowUp");
+    var pLowDown = document.getElementById("parallax-lowDown");
+    var pHighUp = document.getElementById("parallax-highUp");
+
+    pLowUp.style.top = 50 - window.pageYOffset/150 + "%";
+    pLowDown.style.top = 50 + window.pageYOffset/200 + "%";
+    pHighUp.style.top = 50 - window.pageYOffset/50 + "%";
+}
+
+
+// If object in view (Better working for updated browsers)
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {   
+        if (entry.isIntersecting) {
+            entry.target.classList.replace("inactive", "active");
+        }
+    })
+})
+
+
+
+// Event elements
+
+const hiddenElements = document.querySelectorAll(".inactive");
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+// Event Listeners (Type-Effect, _, _)
+
+var executed = false
+document.addEventListener("scroll", () => {
+    var bounding = document.getElementById("type-effect").getBoundingClientRect();
+
+    if (bounding.top >= 0 && bounding.bottom <= window.innerHeight && !executed) {
+        typeEffect();
+        executed = true;
+    }
+});
+
+window.addEventListener("scroll", function() {
+	parallax();	
+});
--- main.js
+++ main.js
@@ -1,62 +1,10 @@
-// Javascript
-
+// Javascript 
 
 // Scroll user to top of page on refresh
 
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
-
-
-
-// Typing effect "Next Generation"
-
-var char = 0;
-var text = 'Next Generation';
-var speed = 70;
-
-function typeEffect() {
-    if (char < text.length) {
-        document.getElementById("type-effect").innerHTML += text.charAt(char);
-        char++;
-        setTimeout(typeEffect, speed);
-    }
-}
-
-
-
-// If object in view (Better working for updated browsers)
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {   
-        if (entry.isIntersecting) {
-            entry.target.classList.replace("inactive", "active");
-        }
-    })
-})
-
-
-
-// Event elements
-
-const hiddenElements = document.querySelectorAll(".inactive");
-hiddenElements.forEach((el) => observer.observe(el));
-
-
-
-// Event Listeners (Type-Effect, _, _)
-
-var executed = false
-document.addEventListener("scroll", () => {
-    var bounding = document.getElementById("type-effect").getBoundingClientRect();
-
-    if (bounding.top >= 0 && bounding.bottom <= window.innerHeight && !executed) {
-        typeEffect();
-        executed = true;
-    }
-});
-
-
 
 // Mobile menu toggle
 
